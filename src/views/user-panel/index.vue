d4fbeb7dda9ff6a1e0daa6ceda6bbd0d7fdf4cb4
WANG SHIHENG <45314441+Aprilerr@users.noreply.github.com>
2023-03-20 11:43:58 +0800

    Initial commit

---
+++ src/views/user-panel/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app-container">
+    <div>
+      hell
+    </div>
+    <div class="avatar">
+      <img :src="avatar" alt="avatar" />
+    </div>
+    <div class="name">hello {{ name }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPanel',
+  computed: {
+    avatar() {
+      return this.$store.getters.avatar
+    },
+    name() {
+      return this.$store.getters.name
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
